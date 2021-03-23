acffb030ff4da8ed18fcd26338557dd4d4033ce4
Francisco Vallarino <fjvallarino@gmail.com>
2021-03-23 17:48:50 -0300

    Use string-interpolate

--- app/Main.hs
+++ app/Main.hs
@@ -1,7 +1,9 @@
 {-# LANGUAGE DeriveAnyClass #-}
 {-# LANGUAGE DeriveGeneric #-}
 {-# LANGUAGE DerivingVia #-}
+{-# LANGUAGE FlexibleContexts #-}
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE QuasiQuotes #-}
 {-# LANGUAGE StandaloneDeriving #-}
 {-# LANGUAGE TypeApplications #-}
 
@@ -9,6 +11,7 @@ module Main where
 
 import Control.Exception (bracket)
 import Data.Store
+import Data.String.Interpolate (i)
 import Data.Text (Text)
 import GHC.Generics
 
@@ -25,6 +28,9 @@ deriving via (MdbxItemStore User) instance MdbxItem User
 openEnvDbi :: IO MdbxEnv
 openEnvDbi = envOpen "./testdb" [MdbxNosubdir, MdbxCoalesce, MdbxLiforeclaim]
 
+userKey :: User -> Text
+userKey user = [i|user-#{_username user}|]
+
 main :: IO ()
 main = bracket openEnvDbi envClose $ \env -> do
   dbi <- dbiOpen env Nothing []
@@ -32,7 +38,7 @@ main = bracket openEnvDbi envClose $ \env -> do
   getItems @Text env dbi ["mark", "dale"] >>= print @[User]
   getItems @Text env dbi ["john", "carl"] >>= print @[User]
   getItems env dbi (_username <$> users) >>= print @[User]
-  getRange @Text env dbi "dale" "mark" >>= mapM_ (print @User)
+  getRange @Text env dbi "user-dale" "user-mark" >>= mapM_ (print @User)
   where
     users = [
       User "mark" "hide",
@@ -41,4 +47,4 @@ main = bracket openEnvDbi envClose $ \env -> do
       User "dale" "done",
       User "carl" "past"
       ]
-    pairs = (\u -> (_username u, u)) <$> users
+    pairs = (\u -> (userKey u, u)) <$> users
